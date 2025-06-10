7b3e0dc369ee5c086e8ef7360889f1e3e51a7093
Ikhsan Satriadi <ikhsansatriadi175@gmail.com>
2025-06-10 07:41:25 +0700

    feat(author): display paper count and translate output
    Adds paper count to author display and translates output to Bahasa.
    Also, changes search to be case insensitive.

--- src/Author/Author.c
+++ src/Author/Author.c
@@ -23,7 +23,8 @@ void print_author(void *data)
     Author *author = (Author *)data;
     if (author)
     {
-        printf("Author: %s\n", author->name);
+        printf("Nama Penulis: %s\n", author->name);
+        printf("Jumlah Paper: %d\n", author->papers->size);
     }
 }
 
@@ -163,7 +164,7 @@ void search_author(BSTreeNode *node, const char *prefix, DLList **author_list)
     size_t prefix_len = strlen(prefix);
 
     // Membandingkan prefix dengan nama author di node
-    int cmp_left = strncmp(prefix, a->name, prefix_len);
+    int cmp_left = strncasecmp(prefix, a->name, prefix_len);
     // Periksa apakah nama author node saat ini dimulai dengan prefix yang dicari
     bool starts_with_prefix = (cmp_left == 0 && strlen(a->name) >= prefix_len);
 
